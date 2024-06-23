0f48654f7e5f230ec1aa01f02567596d56277e16
Heng-Min Tsao <155936846+hengmintsao@users.noreply.github.com>
2024-06-23 00:27:25 -0400

    upload

---
+++ server.js
@@ -0,0 +1,77 @@
+
+/********************************************************************************
+*  WEB322 – Assignment 02
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Hengmin Tsao Student ID: 166494229 Date: 2024 Jun 04
+*
+********************************************************************************/
+
+
+const legoData = require("./modules/legoSets");
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+legoData.Initialize()
+    .then(() => {
+        console.log("Initialize successfully...");
+
+        // GET "/"
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'home.html'));
+        });
+
+        
+        app.get('/about', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'about.html'));
+        });
+
+        
+        
+        app.get('/lego/sets', (req, res) => {
+            const theme = req.query.theme;
+            if (theme) {
+                legoData.getSetsByTheme(theme) // show the specific theme
+                    .then(sets => res.json(sets))
+                    .catch(err => res.status(404).send('Error: ' + err.message));
+            } else {
+                legoData.getAllSets() // if there is not choose specific theme, show all
+                    .then(sets => res.json(sets))
+                    .catch(err => res.status(404).send('Error: ' + err.message));
+            }
+        });
+
+        app.get('/lego/sets/:set_num', (req, res) => {
+            const setNum = req.params.set_num;
+            legoData.getSetByNum(setNum)
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send('Error: ' + err.message));
+        });
+        
+
+        
+        app.use((req, res) => {
+            res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+        });
+
+    })
+    .catch(err => {
+        console.error("Initialization failed: ", err);
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+
